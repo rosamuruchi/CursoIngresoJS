0653d0c6e0160d78d240d31f4a77e00ff71788a0
rosamuruchi <rosa.muruchilaura@gmail.com>
2017-06-28 20:32:31 -0300

    Add files via upload
    subimos los dos primeros archivos

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,7 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var nombre = prompt("Por favor mostra tu nombre", "Natalia Natalia");
+	alert ("su nombre es: " + nombre)
 }
 
