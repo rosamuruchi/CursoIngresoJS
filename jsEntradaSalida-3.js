c24a90f57b71385f093008103483cd17cb0e1f8f
rosamuruchi <rosa.muruchilaura@gmail.com>
2017-06-28 22:06:58 -0300

    Add files via upload

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,16 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var dato =0;
+	dato = "Octavio";
+	alert (dato);
+
+
+//dato=document.getElementById('elNombre').value;
+//alert (dato);
+
+document.getElementById('elNombre').value = "Natalia Natalia"
+
+}
+
