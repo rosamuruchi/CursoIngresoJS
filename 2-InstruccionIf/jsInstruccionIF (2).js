c1fc5d52af454ea6f821ace1771c9a70f30f2e58
rosamuruchi <rosa.muruchilaura@gmail.com>
2017-07-05 22:04:45 -0300

    clase IF

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,6 +1,14 @@
 function Mostrar()
 {
-//tomo la edad  
+//tomo la edad
+	var edad=document.getElementById ('edad').value;
+
+	if (edad>=18)
+	{ 
+
+	 alert ("es mayor de edad");
+
+	}  
 
 
 
--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,17 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad=document.getElementById ('edad').value;
 
+	if (edad>= 18)
+	{ 
+
+		alert ("es mayor de edad");
+
+	}
+	else
+	{
+		alert ("es menor de edad");
+	}
 
 }//FIN DE LA FUNCIÓN
