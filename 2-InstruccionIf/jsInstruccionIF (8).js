df43fd8110e290b3e22003e184c246fb64b6c268
Rocio <rosa.muruchilaura@gmail.com>
2017-07-12 16:59:17 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,7 +1,17 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad=document.getElementById ('edad').value;
+	var estadocivil=document.getElementById ('estadoCivil').value;
 
+	if (edad>17 && estadoCivil!="Soltero")
+	{
+		alert ("es soltero y no es Menor");
+	}
+	else
+	{
+		
+	}
 	
 
 
