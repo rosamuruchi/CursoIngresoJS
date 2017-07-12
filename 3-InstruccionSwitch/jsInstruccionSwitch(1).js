17094637991f9087999fbd8625206e0476aab11d
rosamuruchi <rosa.muruchilaura@gmail.com>
2017-07-12 12:56:05 -0300

    switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,6 +3,28 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+	if (mesDelAño=="Enero")
+	{
+		alert("Que comienzes bien el año");
+	}
+	else
+	{
+		if (mesDelAño== "Marzo")
+		{
+			alert("A clases!!!!");
+		}
+		else
+		{
+			if(mesDelAño=="Julio")
+			{
+				alert("se vienen las vacaciones");
+			}
+			else
+			{
+				alert("Felices Fiestas");
+			}
+		}	
+	}
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -5,7 +5,36 @@ var mesDelAño = document.getElementById('mes').value;
 
 alert (mesDelAño);
 
+switch (mesDelAño)
+ {
+    case "Enero":
+        
+    case "Febrero":
+        
+    case "Marzo":
+        
+    case "Abril":
+        
+    case "Mayo":
+        
+    case "Junio":
+        alert("Falta para el Invierno");
+        break;
+    case "Julio":
+        
+    case "Agosto":
+        alert("Abrigate que hace frio");
+        break;
+    case "Septiembre":
 
+    case "Octubre":
+
+    case "Noviembre":
+        
+    case "Diciembre":
+        alert("Ya pasamos el frio, Hace Calor");
+        break;
+ }
 
 
 }//FIN DE LA FUNCIÓN
