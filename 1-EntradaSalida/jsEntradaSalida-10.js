195f384281352894967e592c5db8e9c40ed06fdf
Rocio <rosa.muruchilaura@gmail.com>
2017-07-12 13:31:39 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,18 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var importe1 = document.getElementById ('importe').value; 
+
+	importe1 = parseInt (importe1);
+
+
+	// saco el -25% de mi importe
+
+	var porcent = parseInt (importe1 * 1.25 );
+
+
+// con esto hago que me muestre en el cuadro "resultado"
+
+document.getElementById ('resultado').value = porcent ;
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,19 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+	var importe = document.getElementById ('sueldo').value; 
+
+	importe = parseInt (importe);
+
+
+	// saco el 10% de mi sueldo
+
+	var porcent = parseInt (importe * 1.1 );
+
+
+// con esto hago que me muestre en el cuadro "resultado"
+
+document.getElementById ('resultado').value = porcent ;
 
 	
 }
