6725aeb8c5e3b5062bd3c10bbd1702db3f0026d3
Rocio <rosa.muruchilaura@gmail.com>
2017-07-12 16:57:52 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,7 +4,26 @@ function Mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
+	switch (mesDelAño)
+	{
+		case "Febrero":
+    	alert("Este mes tiene 28 dias");
+    	break;
+
+    	case "Abril":
+
+    	case "Junio":
+
+    	case "Septiembre":
+
+    	case "Noviembre":
+    	alert("Este mes tiene 30 dias");
+    	break;
+
+    default:
+    	alert ("Este mes tiene 31 dias");
+    	break;
+	}
 	
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -3,6 +3,27 @@ function Mostrar()
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
 
-
+	if (laHora<12 && laHora>6)
+	{
+		alert("Es de manana");
+	}
+	else
+	{
+		if (laHora<20 && laHora>11)
+		{
+			alert("es de Tarde");
+		}
+		else
+		{
+			if (laHora>=25)
+			{
+				alert("la Hora no Existe");
+			}
+			else
+			{
+				alert("es de Noche");
+			}
+		}
+	}
 
 }//FIN DE LA FUNCIÓN
