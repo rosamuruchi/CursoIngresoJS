09f2a5b71a0f69a20f86e91d8882ddf49cf3da28
Rocio <rosa.muruchilaura@gmail.com>
2017-07-17 23:29:48 -0300

    censo y estadisticas
    usando while

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,67 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 
+	var Edad;
+	var Sexo;
+	var EstadoCivil;
+	var Sueldo;
+	var Legajo;
+	var Nacionalidad;
+
+	edad=prompt("Ingrese su edad entre 18 y 90 años inclusive");
+	while (!(edad>17 && edad<=90))
+	{
+		alert ("Ingrese denuevo");
+		edad = prompt("ingrese su edad entre 18 y 90 años inclusive");
+	}
+	document.getElementById('Edad').value=edad
+
+
+
+	sexo=prompt("ingrese M para masculino y F para femenino");
+	while (sexo!="M" && sexo!="F")
+	{
+		alert("Ingrese denuevo");
+		sexo=prompt("Ingrese M para masculino y F para femenino");
+	}
+	document.getElementById('Sexo').value=sexo;
+
+
+	estcivil=prompt("Ingrese 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos");
+	while (estcivil!=1 && estcivil!=2 && estcivil!=3 && estcivil!=4)
+	{
+		alert("Ingrese denuevo");
+		estcivil=prompt("ingrese 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos");
+	}
+	document.getElementById('EstadoCivil').value=estcivil;
+
+
+	SueldoBruto=prompt ("Suedo no menor a 8000");
+	while (SueldoBruto<8000)
+	{
+		alert("ingrese denuevo");
+		SueldoBruto=prompt("Ingrese su Sueldo no menor a 8000");
+	}
+	document.getElementById('Sueldo').value=SueldoBruto;
+
+	numLegajo=prompt("Ingrese su Legajo");
+	while (numLegajo<999)
+	{
+		alert ("Ingrese denuevo");
+		numLegajo=prompt ("Ingrese su Legajo");
+	}
+	document.getElementById('Legajo').value=numLegajo;
+
+
+	nacionalidad=prompt("Ingrese su nacionalidad “A” para argentinos, “E” para extranjeros, “N” para nacionalizados");
+	while(nacionalidad!="A" && nacionalidad!="E" && nacionalidad!="N")
+	{
+		alert("Ingrese denuevo");
+		nacionalidad=prompt ("Ingrese su nacionalidad “A” para argentinos, “E” para extranjeros, “N” para nacionalizados");
+
+	}
+
+
+	document.getElementById('Nacionalidad').value=nacionalidad;
+	
 }
