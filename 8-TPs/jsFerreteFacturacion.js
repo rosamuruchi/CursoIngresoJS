c6189addffa437471513d09bb5374b4f3c7432b5
Rocio <rosa.muruchilaura@gmail.com>
2017-07-20 17:06:11 -0300

    Facturacion de 3 precios

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,60 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+	
+
 function Sumar () 
 {
-	
+	var Precio1;
+	var Precio2;
+	var Precio3;
+
+	Precio1=document.getElementById('PrecioUno').value;
+	Precio2=document.getElementById('PrecioDos').value;
+	Precio3=document.getElementById('PrecioTres').value;
+
+	Precio1=parseInt(Precio1);
+	Precio2=parseInt(Precio2);
+	Precio3=parseInt(Precio3);
+
+	resultado=Precio1+Precio2+Precio3;
+	alert(resultado);
 }
 function Promedio () 
 {
-	
+	var Precio1;
+	var Precio2;
+	var Precio3;
+
+	Precio1=document.getElementById('PrecioUno').value;
+	Precio2=document.getElementById('PrecioDos').value;
+	Precio3=document.getElementById('PrecioTres').value;
+
+	Precio1=parseInt(Precio1);
+	Precio2=parseInt(Precio2);
+	Precio3=parseInt(Precio3);
+
+	resultado=Precio1+Precio2+Precio3
+	promedio=resultado/3
+	alert(promedio);
 }
 function PrecioFinal () 
 {
+	var Precio1;
+	var Precio2;
+	var Precio3;
+
+	Precio1=document.getElementById('PrecioUno').value;
+	Precio2=document.getElementById('PrecioDos').value;
+	Precio3=document.getElementById('PrecioTres').value;
+
+	Precio1=parseInt(Precio1);
+	Precio2=parseInt(Precio2);
+	Precio3=parseInt(Precio3);
+
+	resultado=Precio1+Precio2+Precio3
+	iva=0.21*resultado
+	PrecioFinalmasIva=resultado+iva
+	alert(PrecioFinalmasIva);
 	
 }
