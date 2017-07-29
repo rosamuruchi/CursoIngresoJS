a9a5076c47e0382217f612fb601802874d27ab9d
Rocio <rosa.muruchilaura@gmail.com>
2017-07-28 21:45:49 -0300

    Add files via upload

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,11 +1,51 @@
 function Mostrar()
 {
 
-  //var importef=document.getElementById('importeFinal').value;
-  var importeIva;
+  //Realiazar el algoritmo que lea un importe de un producto por prompt y muestre el importe final por ID
+//sumándole el IVA (21%)
+  /*var importeIva;
+  var Iva=1.21;
   var importf=prompt("Ingrese importe del producto");
   	importf=parseInt(importf);
-	importeIva=importf*1.21;
+	importeIva=importf*Iva;
 
-  document.getElementById('importeFinal').value=importeIva;
+  document.getElementById('importeFinal').value=importeIva;*/
+
+ /* var numero;
+  numero=parseInt(numero);
+  var contador=0;
+  var acumulador=0;
+  var promedio;
+
+  while(contador<10)
+  {
+  	contador=contador+1;
+  	numero=prompt("Ingrese el numero");
+  	
+  	acumulador=acumulador+numero;
+  	acumulador=parseInt(acumulador);
+
+  }
+  promedio=acumulador/contador;
+  document.getElementById('importeFinal').value=promedio;*/
+
+
+var nota;
+var contador=0;
+var notamayor=10;
+var notamenor;
+
+while (contador<10)
+{
+	contador=contador+1;
+	nota=prompt("ingrese la nota");
+	if(nota<notamayor)
+	{
+		nota=notamenor;
+	}
+	console.log("vuelta",contador+1);
+}
+
+ console.log("notamenor"+notamenor);
+
 }
--- 9-Parciales/parcial/uno.js
+++ 9-Parciales/parcial/uno.js
@@ -1,8 +1,90 @@
 
 function Mostrar()
 {
-	var base=document.getElementById('laBase').value;
+	/*var base=document.getElementById('laBase').value;
 	var perimetro;
 	perimetro= base*4;
-	alert(perimetro);
+	alert("el perimetro es de "+perimetro);*/
+
+	/*var resultado;
+	var numero1;
+	var numero2;
+	numero1=prompt("Ingrese un numero");
+	numero2=prompt("Ingrese un numero");
+	numero1=parseInt(numero1);
+	numero2=parseInt(numero2);
+	resultado=numero1+numero2;
+	document.getElementById('laBase').value=resultado;*/
+
+
+//pedir por PROMPT nombre y nacionalidad y mostrar el mensaje en el cuadro de texto "pepe es argentino"//
+	/*var nombre=prompt("ingrese el nombre");
+	var nacionalidad=prompt("ingrese la nacionalidad");
+
+	document.getElementById('laBase').value=nombre+" es "+nacionalidad;*/
+
+//se piden dos numeros mostrar en el cuadro de texto el menor de ellos.
+
+
+
+	
+	/*var nombre1=prompt("ingrese el primer nombre");
+	var numero1=prompt("Ingrese un numero");
+	var nombre2=prompt("ingrese el segundo nombre");
+	var numero2=prompt("Ingrese un numero");
+	
+	if(numero1>numero2)
+	{
+		console.log("el nombre del mayor es "+nombre1);
+	}
+	else
+	{
+		console.log("el nombre del mayor es "+nombre2);
+	}*/
+	var numero1;
+	var numero2;
+	var numero3;
+
+	numero1=prompt("Ingrese un numero");
+	numero2=prompt("Ingrese un numero");
+	numero3=prompt("Ingrese un numero");
+	
+
+	if(numero1<numero2 && numero2<numero3)
+	{
+		alert("el numero del medio es"+ numero2);
+	}
+	else
+	{
+		if(numero1>numero2 && numero1<numero3)
+		{
+			alert("el numero del medio es"+ numero1);
+		}
+		else
+		{
+			if(numero1>numero2 && numero1>numero3)
+			{
+				alert("el numero del medio es"+ numero3);
+			}
+			else
+			{
+				if (numero1>numero2 && numero1>numero1)
+				{
+					alert("el numero del medio es"+ numero2);
+				}
+				else
+				{
+					alert("el numero del medio es"+ numero1);
+				}
+			}
+		}
+
+	}
+
+
+
+
+	
+
+
 }
